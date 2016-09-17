945ed3caf6bbcd8e01777ec0404cfeae9d508549
Simone Civetta <viteinfinite@gmail.com>
2016-09-17 10:27:15 +0200

    Add .json optional extension

--- routes/api.js
+++ routes/api.js
@@ -7,19 +7,19 @@ router.get('/api/v1/conferences', function (req, res) {
     res.json(conferenceStore.availableConferences);
 });
 
-router.get('/api/v1/conferences/:conferenceId/speakers', function (req, res) {
+router.get('/api/v1/conferences/:conferenceId/speakers(.json)?', function (req, res) {
     conferenceStore.getSpeakers(req.param('conferenceId'), function (response) {
         res.json(response);
     });
 });
 
-router.get('/api/v1/conferences/:conferenceId/schedule', function (req, res) {
+router.get('/api/v1/conferences/:conferenceId/schedule(.json)?', function (req, res) {
     conferenceStore.getSchedule(req.param('conferenceId'), function (response) {
         res.json(response);
     });
 });
 
-router.get('/api/v1/conferences/:conferenceId/tracks', function (req, res) {
+router.get('/api/v1/conferences/:conferenceId/tracks(.json)?', function (req, res) {
     conferenceStore.getTracks(req.param('conferenceId'), function (response) {
         res.json(response);
     });
